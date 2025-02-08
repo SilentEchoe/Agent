759bd5660179bf2712a5039d862e0b0ad7a46369
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-02-08 15:55:04 +0800

    LCEL

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from dotenv import load_dotenv
-from langchain_openai import OpenAI
-from langchain_community.llms import Cohere, HuggingFaceHub
-from langchain.model_laboratory import ModelLaboratory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
-#初始化环境变量
-load_dotenv()
+# 创建一个聊天提示模版，其中{topic}是占位符，用于后续插入具体的话题
+prompt = ChatPromptTemplate.from_template("请讲一个关于{topic}的故事")
+# 初始化ChatOpenAI对象，指定使用的模型为"gpt-4"
+model= ChatOpenAI(model="gpt-4")
+output_parser = StrOutputParser()
+'''通过管道操作符(|)连接各个处理步骤，以创建一个处理链
+   其中，prompt用于生成具体的提示文本，model用于根据提示文本生成回应，output_parser用于处理回应并将其转换为字符串'''
 
-OpenAI = OpenAI(temperature=0.1)
-cohere =Cohere(model="command",temperature=0.1)
-huggingface = HuggingFaceHub(repo_id="tiiuae/falcon-7b",model_kwargs={'temperature':0.1})
-#创建一个模型实验室的实例，整合OpenAI,cohere,huggingface
-model_lab = ModelLaboratory.from_llms([OpenAI,cohere,huggingface])
-#使用模型实验室比较不同模型对同一个问题的答案
-model_lab.compare("樱花源于哪个国家")
+chain = prompt| model|output_parser
+# 调用处理链,传入话题"水仙花",执行生成故事的操作
+message = chain.invoke({"topic":"水仙花"})
+print(message)
