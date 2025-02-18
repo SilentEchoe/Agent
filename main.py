97b45261a962fa34df8d544153b9e2334254bf69
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-02-18 14:49:23 +0800

    init

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+import os
+from openai import OpenAI
+from langchain_openai import OpenAI
+from langchain.agents import load_tools
+from langchain.agents import create_react_agent
+from langchain.prompts import PromptTemplate
 
-# 创建一个聊天提示模版，其中{topic}是占位符，用于后续插入具体的话题
-prompt = ChatPromptTemplate.from_template("请讲一个关于{topic}的故事")
-# 初始化ChatOpenAI对象，指定使用的模型为"gpt-4"
-model= ChatOpenAI(model="gpt-4")
-output_parser = StrOutputParser()
-'''通过管道操作符(|)连接各个处理步骤，以创建一个处理链
-   其中，prompt用于生成具体的提示文本，model用于根据提示文本生成回应，output_parser用于处理回应并将其转换为字符串'''
+#初始化模型
+llm = OpenAI(api_key="",base_url="https://api.siliconflow.cn/v1")
 
-chain = prompt| model|output_parser
-# 调用处理链,传入话题"水仙花",执行生成故事的操作
-message = chain.invoke({"topic":"水仙花"})
-print(message)
+#设置工具
+
+template =(
+    '尽你所能回答以下问题。如果能力不够，你可以使用以下工具:\n\n'
+    '{tools}\n\n'
+    'Use the following format:\n\n'
+    'Thought: you should always think about whaht to do \n'
+    'Action: the action to take, should be one of [{tool_names}]\n'
+    'Action Input: the input to the action\n'
+    'Observation: the result of the action\n'
+    '... (this Thought/Action/Action Input/Observation can repeat N times)\n'
+    'Thought: I now know the final answer \n'
+    'Final Answer: the final answer to the original input question\n\n'
+    'Begin!\n\n'
+    'Question: {input}\n'
+    'Thought:{agent_scratchpad}'
+)
+
+#初始化提示词模版
+prompt = PromptTemplate.from_template(template)
+
+
+
+#初始化Agent_
+agent = create_react_agent(llm,tools,prompt)
+
+
+
+
+
+# client = OpenAI(api_key="sk-nsmdcbmcnwdttiwmrpptskpwispbxxmesqenbpbfuhglkfjt", base_url="https://api.siliconflow.cn/v1")
+# response = client.chat.completions.create(
+#     model='deepseek-ai/DeepSeek-R1',
+#     messages=[
+#         {'role': 'user',
+#          'content': "中国大模型行业2025年将会迎来哪些机遇和挑战"}
+#     ],
+#     stream=True
+# )
+#
+# for chunk in response:
+#     print(chunk.choices[0].delta.content, end='')
